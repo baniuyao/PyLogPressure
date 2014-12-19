2f10e07a9a8a24924602f28bf7d7356610776597
frank.yao <frank.yao@vipshop.com>
2014-12-19 11:18:46 +0800

    add feature to support read lines from source file

--- PyLogPressure.py
+++ PyLogPressure.py
@@ -7,29 +7,61 @@ import os
 
 logging.basicConfig(FORMAT='%(asctime)-15s %(message)s', level=logging.DEBUG)
 
-class LogGen(object):
-    #raw_log = '183.61.133.28 - - [15/Apr/2013:12:13:42 +0800] "GET / HTTP/1.1" "0.001" 302 5 "http://www.web.com/show-73685.html?q=0|0|0|1|0|1" "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; InfoPath.2)" 61.178.243.35 www.vipshop.com gd6g2s11'
-    def __init__(self, lps, filename, content):
+
+class LogWriter(object):
+    def __init__(self, lps, dst_file, src_file=None, content=None):
         self.__lps = int(lps)
-        self.__filename = filename
-        #self.__log = (self.raw_log + '\n') * (self.__lps/100)
-        self.__log = (content + '\n') * (self.__lps/100)
+        self.__src_file = src_file
+        self.__dst_file = dst_file
+        self.__content = content
+
     def __get_file_size_mb(self):
         try:
-            file_size = os.path.getsize(self.__filename)
+            file_size = os.path.getsize(self.__dst_file)
         except:
             return 0
         return file_size/1000000
-    def start(self):
+
+    def start_write(self):
+        if self.__src_file is not None:
+            self.start_write_from_src()
+        else:
+            self.start_write_from_content()
+
+    def start_write_from_src(self):
+        src_f = open(self.__src_file)
+        src_f.seek(0, os.SEEK_END)
+        largest_offset = src_f.tell()
+        src_f.seek(0, os.SEEK_SET)
+        while src_f.tell() < largest_offset:
+            if self.__get_file_size_mb() > 1000:
+                dst_f = open(self.__dst_file, 'w')
+            else:
+                dst_f = open(self.__dst_file, 'aw')
+            time.sleep(float(1)/self.__lps)
+            line = ''
+            while True:
+                ret = src_f.read(1)
+                if ret != '\n':
+                    line += ret
+                elif ret == '\n':
+                    break
+            # WRITE TO DST
+            dst_f.write(line + '\n')
+            dst_f.close()
+        src_f.close()
+
+    def start_write_from_content(self):
+        log = (self.__content + '\n') * (self.__lps/100)
         try:
             while True:
                 if self.__get_file_size_mb() > 1000:
-                    o = open(self.__filename, 'w')
+                    o = open(self.__dst_file, 'w')
                 else:
-                    o = open(self.__filename, 'aw')
+                    o = open(self.__dst_file, 'aw')
                 time.sleep(0.01)
-                logging.info('write %s lines log into %s in 0.01s' % (self.__lps/100, self.__filename,))
-                o.write(self.__log)
+                logging.info('write %s lines log into %s in 0.01s' % (self.__lps/100, self.__dst_file,))
+                o.write(log)
                 o.close()
         except KeyboardInterrupt:
             logging.info('quit')
@@ -38,8 +70,9 @@ class LogGen(object):
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-l', '--lps', dest='lps', default=100)
-    parser.add_option('-f', '--file-name', dest='filename', default='/tmp/PyLogPressure.log')
-    parser.add_option('-c', '--content', dest='content', default='127.0.0.1 - - [15/Apr/2013:12:13:42 +0800] "GET / HTTP/1.1" "0.001" 302 5 "http://www.web.com/show-73685.html?q=0|0|0|1|0|1" "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; InfoPath.2)" 127.0.0.1 www.test.com hostname')
+    parser.add_option('-s', '--src-file', dest='src_file')
+    parser.add_option('-d', '--dst-file', dest='dst_file', default='/tmp/PyLogPressure.log')
+    parser.add_option('-c', '--content', dest='content')
     options, agrs = parser.parse_args()
-    log_gen = LogGen(options.lps, options.filename, options.content)
-    log_gen.start()
+    log_generator = LogWriter(options.lps, options.dst_file, options.src_file, options.content)
+    log_generator.start_write()
